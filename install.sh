3907bb94a542404963b75a36afd34ca5b61b1d63
Keunbae Park <ppotpo@gmail.com>
2019-03-07 20:42:39 -0500

    wip

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ curl -sS https://getcomposer.org/installer | php
 
 # sudo mv composer.phar /usr/local/bin/composer
 
+# in case you need to remove php
+# sudo apt-get purge php-common
+sudo apt-get install php-mbstring php-sqlite3 php-xml php-gd php-zip php-curl php-mysql php-cli php php-fpm
+
 composer global require laravel/installer
 composer global require cpriego/valet-linux
 valet install
