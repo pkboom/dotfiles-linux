61ed413b46cf857ea5feb0387b13a3fa33c267d5
Keunbae Park <ppotpo@gmail.com>
2019-07-04 04:40:35 -0400

    wip

--- link.sh
+++ link.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 rm -f $HOME/.zshrc
-ln -s $HOME/linux-setup/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/linux-setup/dotfiles/robbyrussell2.zsh-theme $HOME/
+ln -s $HOME/.dotfiles/dotfiles/.zshrc $HOME/.zshrc
+
+rm -f $HOME/robbyrussell2.zsh-theme
+ln -s $HOME/.dotfiles/dotfiles/robbyrussell2.zsh-theme $HOME/robbyrussell2.zsh-theme
 
 rm -f $HOME/.aliases
-ln -s $HOME/linux-setup/dotfiles/.aliases $HOME/.aliases
+ln -s $HOME/.dotfiles/dotfiles/.aliases $HOME/.aliases
 
 rm -f $HOME/.local/share/applications/web.desktop
-ln -s $HOME/linux-setup/dotfiles/web.desktop $HOME/.local/share/applications/web.desktop
+ln -s $HOME/.dotfiles/dotfiles/web.desktop $HOME/.local/share/applications/web.desktop
 
 rm -f $HOME/.gitconfig
-ln -s $HOME/linux-setup/dotfiles/.gitconfig
+ln -s $HOME/.dotfiles/dotfiles/.gitconfig $HOME/.gitconfig
 
 rm -f $HOME/.gitignore_global
-ln -s $HOME/linux-setup/dotfiles/.gitignore_global
+ln -s $HOME/.dotfiles/dotfiles/.gitignore_global $HOME/.gitignore_global
 
-rm -f /etc/environment
-ln -s $HOME/linux-setup/dotfiles/environment /etc/environment
+sudo rm -f /etc/environment
+sudo ln -s $HOME/.dotfiles/dotfiles/environment /etc/environment
