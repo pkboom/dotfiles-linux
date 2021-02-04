99f343bc970d70839b8ee71f6d56b74feac5a37d
Keunbae Park <pkboom@users.noreply.github.com>
2021-02-04 14:37:36 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -36,6 +36,7 @@ sudo apt-get install -y dconf-editor
 sudo apt install -y gedit
 sudo apt install -y python3-pip
 sudo apt-get install -h rlwrap
+sudo apt-get install -y build-essential
 
 # pbcopy
 sudo apt-get install -y xclip
