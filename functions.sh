0ba43d1eb6d7f955f74397abd152a48b9a3f5127
Keunbae Park <ppotpo@gmail.com>
2021-05-25 03:25:38 -0400

    wip

--- functions.sh
+++ functions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-composer-link() {  
+function composer-link() {  
     composer config repositories.$1 '{"type": "path", "url": "../packages/'$1'"}'
 }
 
-package-new() {
+function package-new() {
     if [ -n "$1" ]; then # If command line argument is present
         dir="$HOME/code/packages"
 
@@ -18,17 +18,19 @@ package-new() {
     fi
 }
 
-wip() {
-    if [ "$1" = "up" ]; then # If command line argument is present
+function wip() {
+    if [ "$1" = "up" ]; then
         git add . && git commit -m 'wip' && git push
     else
         git add . && git commit -m 'wip'
     fi
 }
 
-web() { cd $HOME/code && ls -lh; }
+function web() { 
+    cd $HOME/code && ls -lh 
+}
 
-art() {
+function art() {
     if [ -n "$1" ] && [ $1 = "fresh" ]; then
         php artisan migrate:fresh --seed
     else
@@ -36,7 +38,7 @@ art() {
     fi
 }
 
-watch() {
+function watch() {
     if [ -n "$1" ]; then
         eval "npm run watch-${1}"
     else
@@ -54,7 +56,7 @@ function weather() {
    eval "curl http://wttr.in/${city}"
 }
 
-xdebug() {
+function xdebug() {
     read -p "Is php 8.0 and is xdebug /usr/lib/php/20190902/xdebug.so " -n 1 -r
 
     echo
@@ -90,7 +92,7 @@ EOF"
    fi
 }
 
-db() {
+function db() {
     if [ "$1" = "refresh" ]; then
         mysql -uroot -e "drop database $2; create database $2"
     elif [ "$1" = "create" ]; then
@@ -100,7 +102,7 @@ db() {
     fi
 }
 
-describe() {
+function describe() {
     database=$1
     table=$2
 
@@ -119,7 +121,7 @@ describe() {
     fi
 }
 
-switch_db() {
+function switch_db() {
     PS3="Select DB: "
     connections=("mysql" "postgres")
 
@@ -142,7 +144,7 @@ switch_db() {
     export DATABASE_MANAGEMENT_SYSTEM=$connection
 }
 
-import-format() {
+function import-format() {
     if [ -n "$1" ]; then # If command line argument is present
         cd $1
 
