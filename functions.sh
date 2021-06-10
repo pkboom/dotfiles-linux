d3b467043f26503e5817129bb77d01a1054dc23f
Keunbae Park <ppotpo@gmail.com>
2021-06-10 10:38:01 -0400

    wip

--- functions.sh
+++ functions.sh
@@ -170,11 +170,11 @@ function merge() {
 }
 
 function dock() {
-    if [ $1 = "watch" ]; then
+    if [ "$1" = "watch" ]; then
         docker-compose exec php npm run watch 
-    elif [ $1 = "dump" ]; then
+    elif [ "$1" = "dump" ]; then
         docker-compose exec php php artisan dump-server
-    elif [ $1 = "tinker" ]; then
+    elif [ "$1" = "tinker" ]; then
         docker-compose exec php php artisan tinker
     elif [ -n "$1" ]; then
         docker-compose exec php $@
