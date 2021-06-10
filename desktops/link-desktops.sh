fddc9f9dc33ddaae6d1de8d09c747713e4dd2b95
Keunbae Park <ppotpo@gmail.com>
2021-06-10 13:44:20 -0400

    wip

--- desktops/link-desktops.sh
+++ desktops/link-desktops.sh
@@ -4,6 +4,9 @@ cd $HOME/.dotfiles/desktops
 
 for f in *; do
     if [ -f "$f" ] && [[ "$f" =~ desktop$ ]]; then # 
+        rm -f $HOME/.local/share/applications/$f
         ln -s $HOME/.dotfiles/desktops/$f $HOME/.local/share/applications
     fi
 done
+
+echo 'desktops all linked'
